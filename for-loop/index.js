dcb2f04f66db1e995db9a0a45f768a4c7403087a
musabmemis <musabmemis@gmail.com>
2023-02-21 10:26:50 +0300

    for loop practice

---
+++ for-loop/index.js
@@ -0,0 +1,39 @@
+// const loopDOM = document.querySelector("#loop");
+// let loopEl = document.createElement("span");
+
+// for (let i = 1; i < 15; i++) {
+//   if (i == 3) {
+//     console.log('my favorite', i)
+//     continue;
+//   }
+
+//   if (i == 6) {
+//     console.log('my second favorite', i)
+//     continue;
+//   }
+
+//   if (i == 9) {
+//     console.log('my third favorite', i)
+//     break;
+//   }
+
+//   console.log(i)
+// }
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);   
+//   i++;
+// }
+
+// faktöriyel deneme : 
+
+let answer = 1;
+for (let i = 1; i <=5; i++) {
+    
+    answer *= i;
+}
+
+console.log(answer);
+
+console.log(1*2*3*4*5)
